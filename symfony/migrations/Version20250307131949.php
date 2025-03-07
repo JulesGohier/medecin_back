d71bf102359c6ba15df090b835daad796b11bd51
Romain <roro.tirbois@gmail.com>
2025-03-07 14:22:29 +0100

    modif de migrations

--- symfony/migrations/Version20250307131949.php
+++ symfony/migrations/Version20250307131949.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250306143252 extends AbstractMigration
+final class Version20250307131949 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,15 +22,15 @@ final class Version20250306143252 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE admin (id SERIAL NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_880E0D76E7927C74 ON admin (email)');
-        $this->addSql('CREATE TABLE medecin (num_rpps VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, num_tel VARCHAR(255) DEFAULT NULL, image_name VARCHAR(255) DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, specialite VARCHAR(255) NOT NULL, PRIMARY KEY(num_rpps))');
+        $this->addSql('CREATE TABLE medecin (num_rpps VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, num_tel VARCHAR(255) DEFAULT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, specialite VARCHAR(255) NOT NULL, image_name VARCHAR(255) DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(num_rpps))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_1BDA53C6E7927C74 ON medecin (email)');
-        $this->addSql('CREATE TABLE patient (num_secu_sociale VARCHAR(255) NOT NULL, medecin_perso_num_rpps VARCHAR(255) DEFAULT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, sexe VARCHAR(255) NOT NULL, num_tel VARCHAR(255) DEFAULT NULL, antecedent VARCHAR(255) DEFAULT NULL, date_naissance DATE DEFAULT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, PRIMARY KEY(num_secu_sociale))');
+        $this->addSql('CREATE TABLE patient (num_secu_sociale VARCHAR(255) NOT NULL, medecin_perso VARCHAR(255) DEFAULT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, sexe VARCHAR(255) NOT NULL, num_tel VARCHAR(255) DEFAULT NULL, antecedent VARCHAR(255) DEFAULT NULL, date_naissance DATE DEFAULT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, PRIMARY KEY(num_secu_sociale))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_1ADAD7EBE7927C74 ON patient (email)');
-        $this->addSql('CREATE INDEX IDX_1ADAD7EBC3F2D8EF ON patient (medecin_perso_num_rpps)');
+        $this->addSql('CREATE INDEX IDX_1ADAD7EBD6C24B63 ON patient (medecin_perso)');
         $this->addSql('CREATE TABLE rendez_vous (id SERIAL NOT NULL, rpps_medecin VARCHAR(255) NOT NULL, patient_num_secu_sociale VARCHAR(255) NOT NULL, date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, state VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_65E8AA0A78519CAD ON rendez_vous (rpps_medecin)');
         $this->addSql('CREATE INDEX IDX_65E8AA0A53B8516 ON rendez_vous (patient_num_secu_sociale)');
-        $this->addSql('ALTER TABLE patient ADD CONSTRAINT FK_1ADAD7EBC3F2D8EF FOREIGN KEY (medecin_perso_num_rpps) REFERENCES medecin (num_rpps) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE patient ADD CONSTRAINT FK_1ADAD7EBD6C24B63 FOREIGN KEY (medecin_perso) REFERENCES medecin (num_rpps) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE rendez_vous ADD CONSTRAINT FK_65E8AA0A78519CAD FOREIGN KEY (rpps_medecin) REFERENCES medecin (num_rpps) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE rendez_vous ADD CONSTRAINT FK_65E8AA0A53B8516 FOREIGN KEY (patient_num_secu_sociale) REFERENCES patient (num_secu_sociale) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
@@ -39,7 +39,7 @@ final class Version20250306143252 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE patient DROP CONSTRAINT FK_1ADAD7EBC3F2D8EF');
+        $this->addSql('ALTER TABLE patient DROP CONSTRAINT FK_1ADAD7EBD6C24B63');
         $this->addSql('ALTER TABLE rendez_vous DROP CONSTRAINT FK_65E8AA0A78519CAD');
         $this->addSql('ALTER TABLE rendez_vous DROP CONSTRAINT FK_65E8AA0A53B8516');
         $this->addSql('DROP TABLE admin');
